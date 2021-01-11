db06c0aed590a053bc1d1d8ca3356742537ea062
Anton-Protasov <89267016232@mail.ru>
2021-01-11 22:08:59 +0300

    1.4. Вызов метода с возвращаемым типом. [#395263]метода с аргументами. [#395261]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+/*
+1.4. Вызов метода с возвращаемым типом. [#395263]метода с аргументами. [#395261]
+ */
+public class DummyDic {
+
+    public String engToRus(String eng){
+        return "Неизвестное слово." + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic example = new DummyDic();
+        System.out.println(example.engToRus("example"));
+    }
+}
