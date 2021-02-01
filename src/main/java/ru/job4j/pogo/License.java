98c452628d5d24f0825c540a14e2d421d5f67022
Anton-Protasov <89267016232@mail.ru>
2021-02-01 23:41:49 +0300

    2. Геттеры и Сеттеры. [#395278]

---
+++ src/main/java/ru/job4j/pogo/License.java
@@ -0,0 +1,47 @@
+package ru.job4j.pogo;
+
+import java.util.Date;
+
+/**
+ * 2. Модель данных. [#395278]
+ */
+
+public class License {
+    private String owner;
+    private String model;
+    private String code;
+    private Date created;
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
+
