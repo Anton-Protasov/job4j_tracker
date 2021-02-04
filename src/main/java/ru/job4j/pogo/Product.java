23a9e69196bdc43a55e71aaa6c085c0c544439cc
Anton-Protasov <89267016232@mail.ru>
2021-02-04 23:14:54 +0300

    4. Сравнение моделей. Метод equals [example]

--- src/main/java/ru/job4j/pogo/Product.java
+++ src/main/java/ru/job4j/pogo/Product.java
@@ -1,13 +1,30 @@
 package ru.job4j.pogo;
 
+import java.util.Objects;
+
 /**
  * 3. Массивы и модели. [#395279]
  * 3.1. Массив с пустыми ячейками [#395281]
+ * 4. Сравнение моделей. Метод equals [#395280]
  */
 
 public class Product {
     private String name;
     private int count;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return count == product.count &&
+                Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, count);
+    }
 
     public Product(String name, int count) {
         this.name = name;
