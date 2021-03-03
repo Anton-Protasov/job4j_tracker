7fa8bfee98c4eec102c69711b6b59d26a79d90a2
Anton-Protasov <89267016232@mail.ru>
2021-03-03 21:54:03 +0300

    3. Интерфейс Input [#395209]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,20 +1,34 @@
 package ru.job4j.poly;
 
+import java.util.Scanner;
+
 public class Bus implements Transport {
 
 
     @Override
     public void drive() {
+        System.out.println("Куда едем?");
+        Scanner input = new Scanner(System.in);
+        String destination = input.nextLine();
 
     }
 
     @Override
     public void passengers (int pas) {
+        System.out.println("Сколько пассажиров едет?");
+        Scanner input = new Scanner(System.in);
+        pas = Integer.valueOf(input.nextLine());
 
     }
 
     @Override
     public int cost(int petrol) {
-        return 0;
+        System.out.println("Сколько литров топлива заправить?");
+        Scanner inputPetrol = new Scanner(System.in);
+        petrol = Integer.valueOf(inputPetrol.nextLine());
+        System.out.println("Сколько стоит один литр?");
+        Scanner inputMoney = new Scanner(System.in);
+        int money = Integer.valueOf(inputMoney.nextLine());
+        return petrol * money;
     }
 }
