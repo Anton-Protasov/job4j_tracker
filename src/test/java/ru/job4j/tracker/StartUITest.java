498867bb9cc44c8d44ba0228f72747e45aa5475a
Anton-Protasov <89267016232@mail.ru>
2021-03-14 14:02:52 +0300

    5.1. Тестирование. Подготовка данных. [#395216]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -44,7 +44,7 @@ public class StartUITest {
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertThat(deleted.getName(), is(nullValue()));
+        assertThat(deleted, is(nullValue()));
 
 
         /*
