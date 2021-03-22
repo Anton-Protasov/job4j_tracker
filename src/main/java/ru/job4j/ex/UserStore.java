d4728a56ddd9df43b90d893fcb2c4c5a328a2b35
Anton-Protasov <89267016232@mail.ru>
2021-03-23 00:54:15 +0300

    0.5. Иерархия исключений и множественный catch. [#395230]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,47 @@
+package ru.job4j.ex;
+
+/**
+ * 0.5. Иерархия исключений и множественный catch. [#395230]
+ * findUser - если пользователя не нашли в списке, то кинуть исключение UserNotFoundException.
+ * validate - если пользователь не валидный или если имя пользователя состоит из менее трех символов,
+ * то кинуть исключение UserInvalidException
+ */
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User user = null;
+        for (int i = 0; i < users.length; i++) {
+            if(!login.equals(users[i].getUsername())) {
+                throw new UserNotFoundException("User not found");
+            } else {
+                user = users[i];
+                break;
+            }
+        }
+        return user;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if(!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User invalid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+        User user = findUser(users, "Petr Arsentev");
+        if (validate(user)) {
+            System.out.println("This user has an access");
+        }
+        } catch (UserInvalidException ui) {
+            ui.printStackTrace();
+        } catch (UserNotFoundException un) {
+            un.printStackTrace();
+        }
+    }
+}
