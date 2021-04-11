a159744bbaae3829d1cdfe134ed39f314dbee103
Anton-Protasov <89267016232@mail.ru>
2021-04-11 22:33:11 +0300

    2. Singleton. [#395243]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+/**
+ *  класс SingleTracker должен реализовывать шаблон singleton
+ */
+
+public class SingleTracker {
+    private Tracker tracker = new Tracker();
+    private static SingleTracker example;
+
+    private SingleTracker(){
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key){
+        return tracker.findByName(key);
+    }
+
+    public boolean replace(int id, Item item){
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id){
+        return tracker.delete(id);
+    }
+
+    public static SingleTracker getExample() {
+        return example = new SingleTracker();
+    }
+}
