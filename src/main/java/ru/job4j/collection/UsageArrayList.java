3f4a3522cf3d05138884f74070a35b900408b48d
Anton-Protasov <89267016232@mail.ru>
2021-05-03 20:18:43 +0300

    0. Коллекции, ArrayList, List, Обобщения [#395192]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
  */
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> name = new ArrayList<String>();
+        ArrayList<String> name = new ArrayList<>();
         name.add("Petr");
         name.add("Ivan");
         name.add("Stepan");
-        for (Object value: name) {
+        for (String value: name) {
             System.out.println(value);
         }
     }
