d1b37be24582e5a0796987c721c36855c077fd4b
Anton-Protasov <89267016232@mail.ru>
2021-05-09 17:37:47 +0300

    3. Модели данных и HashSet. [#395201]

---
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * 3. Модели данных и HashSet. [#395201]
+ * В банковской системе, есть рассылка.
+ * Клиент подписался на разные рассылки. Может быть такая ситуация,
+ * что клиент должен получить сразу несколько писем.
+ * Чтобы система не спамила клиента, там нужно оставить только уникальных клиентов для отправки.
+ * То есть если клиент должен получить уведомление по разным подпискам, мы будем упаковывать их в одно письмо.
+ * Решение этой задачи сводиться к преобразованию List в Set. HashSet устранит дубликаты клиентов банка самостоятельно.
+ */
+
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+        HashSet<Account> rsl = new HashSet<>();
+        for(Account account: accounts) {
+            rsl.add(account);
+        }
+        return rsl;
+    }
+}
