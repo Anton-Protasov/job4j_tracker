8534f93fb63afb244298ed6cb7ef60500da4822d
Anton-Protasov <89267016232@mail.ru>
2021-05-09 16:07:59 +0300

    1. Уникальные задачи. [#395199]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * 1. Уникальные задачи. [#395199]
+ * extractNumber - Метод, который извлечет все номера и уберет дубликаты.
+ */
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        for(Task task: list){
+            numbers.add(task.getNumber());
+        }
+        return numbers;
+    }
+}
---
+++ src/main/java/ru/job4j/collection/Task.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+/**
+ * 1. Уникальные задачи. [#395199]
+ */
+
+public class Task {
+    private String number;
+    private String description;
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
