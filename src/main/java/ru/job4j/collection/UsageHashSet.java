6962b44dc95f1e5941f9489f3d4dd8da93838882
Anton-Protasov <89267016232@mail.ru>
2021-05-09 15:37:29 +0300

    0. Set, HashSet, Iterator. [#395202]

---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+/**
+ * 0. Set, HashSet, Iterator. [#395202]
+ */
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<>();
+        autos.add("Lada");
+        autos.add("BMV");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        for(String name: autos) {
+            System.out.println(name);
+        }
+    }
+}
