fa27dc5b40ee353b9a040e155de14a47143017ea
Anton-Protasov <89267016232@mail.ru>
2021-06-05 23:48:07 +0300

    3. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -1,36 +1,67 @@
 package ru.job4j.bank;
 
+/*
+ * 3. Банковские переводы [#10038]
+ */
+
 import java.util.Objects;
 
 /**
- * 3. Банковские переводы [#10038]
- * Модель банковского счета содержит поля: баланс и реквизиты.
+ * Class is a model of a client`s bank account
+ * @author Anton Protasov
+ * @version 1.0
  */
-
 public class Account {
     private String requisite;
     private double balance;
 
+    /**
+     * @param requisite - the ID of an client`s account
+     * @param balance - the current quantity of money on an account
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * the method allows to get a requisite of an account
+     * @return of a account`s requisite
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * the method allows to set a requisite of an account
+     * @param requisite - a requisite should be indicated as input data of the method
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * the method allows to get the current balance of an account
+     * @return of the current balance on the account
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * the method allows to set the current balance of an account
+     * @param balance - a balance should be indicated as input data of the method
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
+    /**
+     * equals and hashCode are standard overridden methods
+     * in order to be allow to use an object of this class
+     * in operations with a comparison.
+     * These methods were got by using a feature that integrated in IDEA
+     */
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -1,37 +1,67 @@
 package ru.job4j.bank;
 
+/*
+ * 3. Банковские переводы [#10038]
+ */
+
 import java.util.Objects;
 
 /**
- * 3. Банковские переводы [#10038]
- * Модель Пользователя содержит поля: номер паспорта и ФИО.
+ * Class is a model of a bank`s client
+ * @author Anton Protasov
+ * @version 1.0
  */
-
 public class User {
     private String passport;
     private String username;
 
+    /**
+     * @param passport - the ID of an bank`s client
+     * @param username - the username of an bank`s client
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * the method allows to get a client`s passport
+     * @return of a client`s passport
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * the method allows to set a client`s passport
+     * @param passport - a passport should be indicated as input data of the method
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * the method allows to get a client`s username
+     * @return of a client`s username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * the method allows to set a client`s username
+     * @param username - an username should be indicated as input data of the method
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * equals and hashCode are standard overridden methods
+     * in order to be allow to use an object of this class
+     * in operations with a comparison.
+     * These methods were got by using a feature that integrated in IDEA
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
